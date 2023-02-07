6fde40fc4c0c4424eb23a712ef68e38eb8d33e02
Rianna3 <flnf58@durham.ac.uk>
2023-02-07 13:31:59 +0000

    auth.ts

--- frontend/src/auth/auth.ts
+++ frontend/src/auth/auth.ts
@@ -1,2 +1,50 @@
-// to fill out
-export function auth() {}
+import axios from 'axios';
+
+
+const BACKEND = "localhost:3000"
+interface JWToken {
+    jsonWebToken: string;
+}
+export async function auth(ssoString: string) {
+    const resp = await fetch(BACKEND + "/login", {
+        method: "POST",
+        body: JSON.stringify({ ssoString })
+    })
+    const data: JWToken = await resp.json()
+    localStorage.setItem('loginData', data.jsonWebToken);
+
+    const request = axios.create({
+        baseURL: BACKEND,
+        timeout: 5000
+    })
+
+    request.interceptors.request.use(
+        (config) => {
+            const token = localStorage.getItem('loginData')
+
+            if (token) config.headers.Authorization = token;
+            return config
+        },
+        (error) => Promise.reject(error)
+    )
+
+    const express = require('express')
+    const router = express.Router()
+    request.interceptors.response.use(
+        (response) => {
+            const { status, data } = response
+            if (data.success) {
+                return data
+            } else {
+                if (status === 401) {
+                    router.push({ name: "Login" })
+                }
+                return Promise.reject(data.message)
+            }
+        },
+        error => {
+            return Promise.reject(error)
+        }
+    )
+
+}
