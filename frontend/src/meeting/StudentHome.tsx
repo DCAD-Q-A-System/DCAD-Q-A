531f0505711cfa5635ec24072237735d1ace2b5c
Harrison21 <zvnm27@durham.ac.uk>
2023-02-08 16:08:01 +0000

    Change html to tsx

---
+++ frontend/src/meeting/StudentHome.tsx
@@ -0,0 +1,65 @@
+export function StudentHome() {
+    return (
+      <>
+        <div class="div">
+            <div class="div-2">
+            <picture>
+                <source
+                srcset="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F05c4628b6c9f4e4d844775517201f6a5?format=webp&width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F05c4628b6c9f4e4d844775517201f6a5?format=webp&width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F05c4628b6c9f4e4d844775517201f6a5?format=webp&width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F05c4628b6c9f4e4d844775517201f6a5?format=webp&width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F05c4628b6c9f4e4d844775517201f6a5?format=webp&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F05c4628b6c9f4e4d844775517201f6a5?format=webp&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F05c4628b6c9f4e4d844775517201f6a5?format=webp&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F05c4628b6c9f4e4d844775517201f6a5"
+                type="image/webp"
+                />
+                <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F05c4628b6c9f4e4d844775517201f6a5"
+                srcset="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F05c4628b6c9f4e4d844775517201f6a5?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F05c4628b6c9f4e4d844775517201f6a5?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F05c4628b6c9f4e4d844775517201f6a5?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F05c4628b6c9f4e4d844775517201f6a5?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F05c4628b6c9f4e4d844775517201f6a5?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F05c4628b6c9f4e4d844775517201f6a5?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F05c4628b6c9f4e4d844775517201f6a5?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F05c4628b6c9f4e4d844775517201f6a5"
+                class="image"
+                />
+            </picture>
+            <div class="builder-image-sizer image-sizer"></div>
+            </div>
+        </div>
+        <style>
+            {`
+            .div {
+            display: flex;
+            flex-direction: column;
+            max-width: 1920px;
+            justify-content: flex-start;
+            align-items: flex-start;
+            padding-top: 340px;
+            padding-right: 710px;
+            padding-bottom: 340px;
+            padding-left: 710px;
+            background-image: url("https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fbaf11bbc168d408b857d0a142fb6bb44");
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+            }
+            .div-2 {
+            display: flex;
+            align-self: stretch;
+            position: relative;
+            min-width: 20px;
+            min-height: 20px;
+            max-width: 500px;
+            }
+            .image {
+            object-fit: cover;
+            object-position: center;
+            position: absolute;
+            height: 100%;
+            width: 100%;
+            top: 0;
+            left: 0;
+            }
+            .image-sizer {
+            width: 100%;
+            padding-top: 80%;
+            pointer-events: none;
+            font-size: 0;
+            }`}
+        </style>
+      </>
+    );
+  }
+  
