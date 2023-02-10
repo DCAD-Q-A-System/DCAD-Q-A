a74672cc54b9292e3781f0d093ce149e7de93f14
Harrison21 <zvnm27@durham.ac.uk>
2023-02-10 11:34:02 +0000

    Fix errors and build new functions

--- frontend/src/Login/Login.tsx
+++ frontend/src/Login/Login.tsx
@@ -3,56 +3,56 @@ import * as React from "react";
 export function Login(props: any) {
   return (
     <>
-      <div class="div">
-        <div class="div-2">
-          <div class="div-3">
-            <div class="builder-columns div-4">
-              <div class="builder-column column">
-                <div class="div-5">
+      <div className="div">
+        <div className="div-2">
+          <div className="div-3">
+            <div className="builder-columns div-4">
+              <div className="builder-column column">
+                <div className="div-5">
                   <picture>
                     <source
-                      srcset="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?format=webp&width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?format=webp&width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?format=webp&width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?format=webp&width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?format=webp&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?format=webp&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?format=webp&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b"
+                      srcSet="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?format=webp&width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?format=webp&width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?format=webp&width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?format=webp&width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?format=webp&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?format=webp&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?format=webp&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b"
                       type="image/webp"
                     />
                     <img
                       loading="lazy"
                       src="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b"
-                      srcset="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b"
-                      class="image"
+                      srcSet="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F266920c105df499e8fdfb93060b2484b"
+                      className="image"
                     />
                   </picture>
-                  <div class="builder-image-sizer image-sizer"></div>
+                  <div className="builder-image-sizer image-sizer"></div>
                 </div>
               </div>
-              <div class="builder-column column-2">
-                <div class="welcome-to-the-online-q-a-syst">
+              <div className="builder-column column-2">
+                <div className="welcome-to-the-online-q-a-syst">
                   Welcome to the online Q&A system
                 </div>
               </div>
             </div>
           </div>
-          <div class="div-6">
-            <div class="builder-columns div-7">
-              <div class="builder-column column-3">
-                <div class="div-8">
-                  <div class="div-9">
-                    <div class="builder-columns div-10">
-                      <div class="builder-column column-4">
-                        <div class="div-11">Login as Student</div>
+          <div className="div-6">
+            <div className="builder-columns div-7">
+              <div className="builder-column column-3">
+                <div className="div-8">
+                  <div className="div-9">
+                    <div className="builder-columns div-10">
+                      <div className="builder-column column-4">
+                        <div className="div-11">Login as Student</div>
                       </div>
                     </div>
                   </div>
-                  <div class="div-12">
-                    <div class="builder-columns div-13">
-                      <div class="builder-column column-5">
-                        <div class="div-14">Login as Panellist</div>
+                  <div className="div-12">
+                    <div className="builder-columns div-13">
+                      <div className="builder-column column-5">
+                        <div className="div-14">Login as Panellist</div>
                       </div>
                     </div>
                   </div>
-                  <div class="div-15">
-                    <div class="builder-columns div-16">
-                      <div class="builder-column column-6">
-                        <div class="div-17">Join as Guest</div>
+                  <div className="div-15">
+                    <div className="builder-columns div-16">
+                      <div className="builder-column column-6">
+                        <div className="div-17">Join as Guest</div>
                       </div>
                     </div>
                   </div>
--- frontend/src/Login/Login2.tsx
+++ frontend/src/Login/Login2.tsx
@@ -1,51 +1,69 @@
+import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { setData } from "../store/loginSlice";
+
 export function Login2() {
+  const dispatch = useAppDispatch();
+  const loginData = useAppSelector((state) => state.loginReducer.data);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = async (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Hash function implement later
+    const res = await fetch("http://127.0.0.1:8080/login", {
+      method:"POST",
+      body:JSON.stringify({username,password})
+    })
+    const jwt = await res.json()
+    dispatch(setData({data:jwt}));
+  }
   return (
     <>
-      <div class="div">
-        <div class="div-2">
-          <div class="div-3">
-            <div class="builder-columns div-4">
-              <div class="builder-column column">
-                <div class="div-5">
+      <div className="div">
+        <div className="div-2">
+          <div className="div-3">
+            <div className="builder-columns div-4">
+              <div className="builder-column column">
+                <div className="div-5">
                   <picture>
                     <source
-                      srcset="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?format=webp&width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?format=webp&width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?format=webp&width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?format=webp&width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?format=webp&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?format=webp&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?format=webp&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306"
+                      srcSet="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?format=webp&width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?format=webp&width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?format=webp&width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?format=webp&width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?format=webp&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?format=webp&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?format=webp&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306"
                       type="image/webp"
                     />
                     <img
                       loading="lazy"
                       src="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306"
-                      srcset="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306"
-                      class="image"
+                      srcSet="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F26a67cd96e9a4fdcab5916c7b4029306"
+                      className="image"
                     />
                   </picture>
-                  <div class="builder-image-sizer image-sizer"></div>
+                  <div className="builder-image-sizer image-sizer"></div>
                 </div>
               </div>
-              <div class="builder-column column-2">
-                <div class="welcome-to-the-online-q-a-syst">
+              <div className="builder-column column-2">
+                <div className="welcome-to-the-online-q-a-syst">
                   Welcome to the online Q&A system
                 </div>
               </div>
             </div>
           </div>
-          <div class="div-6">
-            <div class="builder-columns div-7">
-              <div class="builder-column column-3">
-                <div class="div-8">
-                  <div class="div-9">
-                    <div class="div-10">Username:</div>
-                    <div class="div-11"></div>
+          <form onSubmit={(e)=>handleSubmit} className="div-6">
+            <div className="builder-columns div-7">
+              <div className="builder-column column-3">
+                <div className="div-8">
+                  <div className="div-9">
+                    <label className="div-10">Username:</label>
+                    <input type="text" onChange={(e)=>setPassword(e.target.value)} className="div-11"/>
                   </div>
-                  <div class="div-12">
-                    <div class="div-13">Password:</div>
-                    <div class="div-14"></div>
+                  <div className="div-12">
+                    <label className="div-13">Password:</label>
+                    <input type="password" onChange={(e)=>setPassword(e.target.value)} className="div-14"/>
                   </div>
-                  <div class="div-15">Login</div>
+                  <button type="submit" className="div-15">Login</button>
                 </div>
               </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
       <style>{`
