721cf6c67f86e92e8800155b3c4942c7495c8435
Andrew Li <andrewfs2003@gmail.com>
2023-02-11 22:38:23 +0000

    imported  Login2 in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Login } from "./login/Login";
+import { Login2 } from "./login/Login2"
 import { MeetingDetails } from "./meeting/MeetingDetails";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
