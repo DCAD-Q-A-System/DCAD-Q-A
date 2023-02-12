b81394c79fcdb0c031b763bc58791d608ed55607
Rianna3 <flnf58@durham.ac.uk>
2023-02-12 16:06:14 +0000

    EmojiComponent

--- frontend/src/chat/EmojiComponent.tsx
+++ frontend/src/chat/EmojiComponent.tsx
@@ -1,4 +1,44 @@
-export function EmojiComponent(){
+import React, { useState } from 'react';
+import Picker, { EmojiStyle, SkinTones, Theme } from 'emoji-picker-react'
 
-    return <></>
+type EmojiObjectProps = {
+
+    activeSkinTone: SkinTones;
+    unified: string;
+    unifiedWithoutSkinTone: string;
+    emoji: string;
+    names: string[];
+    getImageUrl: (emojiStyle: EmojiStyle) => string;
+
 }
+
+type EmojiComponentProps = {
+    inputStr: string
+    setInputStr: React.Dispatch<React.SetStateAction<string>>
+    showPicker: boolean
+    setShowPicker: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export function EmojiComponent({ inputStr, setInputStr, showPicker, setShowPicker }: EmojiComponentProps) {
+
+
+
+    const onEmojiClick = (EmojiObject: EmojiObjectProps) => {
+        setInputStr(prevInput => prevInput + EmojiObject.emoji)
+        setShowPicker(false)
+    }
+
+    return (
+        <>
+
+            <img
+                className='emoji-icon'
+                src="https://icons.getbootstrap.com/assets/icons/emoji-smile.svg"
+                onClick={() => setShowPicker(!showPicker)} />
+            {showPicker && <Picker
+                onEmojiClick={onEmojiClick}
+                theme={Theme.AUTO} />}
+        </>
+    );
+}
+
