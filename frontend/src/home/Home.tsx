7507949d19fa5104be4d17221a5930cae31a96af
Rianna3 <flnf58@durham.ac.uk>
2023-02-22 18:17:18 +0000

    meetinglist and home for student and panellist

--- frontend/src/home/Home.tsx
+++ frontend/src/home/Home.tsx
@@ -1,46 +1,44 @@
-import React from "react";
-
 import "./Home.css";
-import current from "../image/current_meeting.png";
-import create from "../image/create_meeting.png";
 import { useAppSelector } from "../store/hooks";
 import { useNavigate } from "react-router-dom";
 import { USER_TYPE } from "../utils/enums";
+import {AiOutlinePlus} from 'react-icons/ai'
+import { BsClockFill } from 'react-icons/bs'
 
 export function Home() {
   const loginData = useAppSelector((state) => state.loginReducer.data);
   const navigate = useNavigate();
+
+  window.addEventListener('resize', function () {
+    const background = document.getElementById('background-image');
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    if (background !== null) {
+        background.style.width = `${width}px`;
+        background.style.height = `${height}px`;
+    }
+
+});
+
   return (
     <>
-      <div className="div">
-        <div className="builder-columns div-2">
-          <div className="builder-column column">
-            <div className="div-3">
-              <picture>
-                <img
-                  onClick={() => navigate("/meeting-list")}
-                  loading="lazy"
-                  src={current}
-                  className="image"
-                />
-              </picture>
-              <div className="builder-image-sizer image-sizer"></div>
-            </div>
-          </div>
-          {loginData &&
+            <div id="background-image" >
+                <div className="container">
+                    <div className="box" onClick={()=>{navigate('/meeting-list')}}>
+                        <BsClockFill className="icon" />
+                        <div className="word">Current Meeting</div>
+                    </div>
+                               {loginData &&
             (loginData.type === USER_TYPE.PANELLIST ||
               loginData.type === USER_TYPE.ADMIN) && (
-              <div className="builder-column column-2">
-                <div className="div-4">
-                  <picture>
-                    <img loading="lazy" src={create} className="image" />
-                  </picture>
-                  <div className="builder-image-sizer image-sizer"></div>
+                    <div className="box" onClick={()=>{navigate('/meeting-detail')}}>
+                        <AiOutlinePlus className="icon" />
+                        <div className="word">Creating Meeting</div>
+                    </div>
+           )}
+
                 </div>
-              </div>
-            )}
-        </div>
-      </div>
+            </div>
     </>
   );
 }
--- frontend/src/meeting/meeting/MeetingList.tsx
+++ frontend/src/meeting/meeting/MeetingList.tsx
@@ -1,56 +1,138 @@
-import { useEffect, useState } from "react";
-import { useAppSelector } from "../../store/hooks";
-import { credentialFetch } from "../../utils/credential_fetch";
-import { HTTP_METHODS } from "../../utils/http_methods";
-import { MeetingIds } from "../../utils/interfaces";
-import { GET_ALL_MEETINGS, GET_ALL_MESSAGES } from "../../utils/paths";
-import { JoinMeeting } from "../components/join_meeting/JoinMeeting";
-import { MeetingItem } from "../components/MeetingItem";
-import "./MeetingList.css";
+import './MeetingList.css'
+import 'bootstrap'
+import { useAppSelector } from '../../store/hooks';
+import { useNavigate } from 'react-router-dom'
+import { EDIT_MEETING } from '../../utils/paths';
+import { JoinMeeting } from '../components/join_meeting/JoinMeeting';
 
-export function MeetingList() {
-  const [ids, setIds] = useState<MeetingIds>({ ids: [] });
+
+export function MeetingListStudent() {
   const loginData = useAppSelector((state) => state.loginReducer.data);
-  useEffect(() => {
-    const fetchMeetings = async () => {
-      const res = await credentialFetch(
-        `${GET_ALL_MEETINGS}?userId=${loginData?.userId || ""}`,
-        HTTP_METHODS.GET
-      );
-      if (res.status === 200) {
-        const data: MeetingIds = res.data;
-        setIds(data);
-      } else {
-        alert("something went wrong fetching meetings");
-      }
-    };
-    fetchMeetings();
-  }, []);
+  const navigate = useNavigate();
+
+  window.addEventListener('resize', function () {
+    const background = document.getElementById('background-image');
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    if (background !== null) {
+      background.style.width = `${width}px`;
+      background.style.height = `${height}px`;
+    }
+  });
 
   return (
     <>
-      <div className="panDiv">
-        <div className="panDiv-2">
-          {ids.ids.length > 0 ? (
-            ids.ids.map(({ name, id, startTime, endTime }) => {
-              return (
-                <MeetingItem
-                  key={id}
-                  id={id}
-                  name={name}
-                  startTime={startTime}
-                  endTime={endTime}
-                />
-              );
-            })
-          ) : (
-            <div>
-              <p>Can't fetch meetings at the moment</p>
-              <p>Try again later</p>
+      <div id="background-image" >
+        <div className="Container">
+          <div className='box'>
+            <div className='box1'>
+              <div className='box2-student'>
+                <p className='content'>
+                  ID : 123456789
+                </p>
+              </div>
+              <button className='join'>
+                <p className='content-1'>
+                  Join
+                </p>
+              </button>
             </div>
-          )}
+            <div className='box1'>
+              <div className='box2-student'>
+                <p className='content'>
+                  ID : 987654321
+                </p>
+              </div>
+              <button className='join'>
+                <p className='content-1'>
+                  Join
+                </p>
+              </button>
+            </div>
+            <div className='box1'>
+              <div className='box2-student'>
+                <p className='content'>
+                  ID : 123456789
+                </p>
+              </div>
+              <button className='join'>
+                <p className='content-1'>
+                  Join
+                </p>
+              </button>
+            </div>
+            <div className='box1'>
+              <div className='box2-student'>
+                <p className='content'>
+                  ID : 987654321
+                </p>
+              </div>
+              <button className='join'>
+                <p className='content-1'>
+                  Join
+                </p>
+              </button>
+            </div>
+          </div>
+
         </div>
       </div>
     </>
-  );
+  )
 }
+export function MeetingListPanellist() {
+  const loginData = useAppSelector((state) => state.loginReducer.data);
+  const navigate = useNavigate();
+
+  window.addEventListener('resize', function () {
+    const background = document.getElementById('background-image');
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    if (background !== null) {
+      background.style.width = `${width}px`;
+      background.style.height = `${height}px`;
+    }
+  });
+
+  return (
+    <>
+      <div id="background-image" >
+        <div className="Container">
+          <div className='box'>
+            <div className='box1'>
+              <div className='box2'>
+                <p className='content'>
+                  ID : 123456789
+                </p>
+              </div>
+              <JoinMeeting meetingId={''} userId={''} />
+              <button className='edit'>
+                <p className='content-1'>
+                  Edit
+                </p>
+              </button>
+            </div>
+            <div className='box1'>
+              <div className='box2'>
+                <p className='content'>
+                  ID : 987654321
+                </p>
+              </div>
+              <JoinMeeting meetingId={''} userId={''} />
+              <button className='edit' onClick={() => navigate('/edit-meeting')}>
+                <p className='content-1'>
+                  Edit
+                </p>
+              </button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+
+
+
