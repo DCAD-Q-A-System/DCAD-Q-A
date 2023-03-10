0e1177ba41fe169846acc264fbbfdeb361fbfa42
johnny-stevie <johnstevenwall@gmail.com>
2023-03-10 11:12:30 +0000

    - fix panopto autoplay issue

--- frontend/src/meeting/components/iframe/Iframe.tsx
+++ frontend/src/meeting/components/iframe/Iframe.tsx
@@ -1,6 +1,11 @@
 import React from "react";
-
+import { PANOPTO_REGEX } from "../../../utils/regex";
 export function Iframe({ link }: { link: string }) {
+  if (link.match(PANOPTO_REGEX)) {
+    return (
+      <iframe src={link} className="responsive-iframe" allowFullScreen></iframe>
+    );
+  }
   return (
     <iframe
       src={link}
